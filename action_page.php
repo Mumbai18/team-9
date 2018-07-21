6bf019f27d608ec9f34999711fbbebf38eac71d1
Abhijeet Salunkhe <abhijeet21895@gmail.com>
2018-07-21 18:46:51 +0530

    Final Registration

--- action_page.php
+++ action_page.php
@@ -7,8 +7,8 @@ $mobile=$_POST['phoneno'];
 
 if(isset($email) || isset($password))
 {
-	$sql="INSERT INTO `stud_registration` (`stud_mobile`,`stud_email`,`stud_pwd`) VALUES ($mobile,$email,$password)";
-			$result=mysqli_query($con,$sql);
+	$sql="INSERT INTO `stud_registration` (`stud_mobile`,`stud_email`,`stud_pwd`) VALUES ('$mobile','$email','$password')";
+			mysqli_query($con,$sql) or die(mysqli_error($con));
       echo "<script type='text/javascript'>window.location.href='login.php';</script>";
       
 }else {
