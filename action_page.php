673d09ac31955cd55abe85393b82f65b629185d3
Riya Karia <2015riya.karia@ves.ac.in>
2018-07-21 13:18:20 +0530

    changes

--- action_page.php
+++ action_page.php
@@ -4,12 +4,11 @@ include_once 'connect.php';
 $email=$_POST['email'];
 $password=$_POST['password'];
 $mobile=$_POST['phoneno'];
-
 if(isset($email) || isset($password))
 {
-	$sql="INSERT into `stud_registration` (`stud_mobile`,`stud_email`,`stud_pwd`) VALUES ($mobile,$email,$password)";
+	$sql="INSERT into `stud_registration` (`stud_mobile`,`stud_email`,`stud_pwd`) VALUES ('$mobile','$email','$password')";
 			$result=mysqli_query($con,$sql);
-			//echo "hello";
+			
       echo "<script type='text/javascript'>window.location.href='login.php';</script>";
       
 }else {
