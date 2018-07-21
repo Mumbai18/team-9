12e29c0d199a9a8870f8a2feee0bd4694cc78584
Abhijeet Salunkhe <abhijeet21895@gmail.com>
2018-07-21 20:49:37 +0530

    Changes

--- connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-$connect_error='Sorry';
-//$con=mysqli_connect("localhost:3307","root","","educon")or die($connect_error);
-$con=mysqli_connect("localhost","root","root","educon") or die($connect_error);
-
-?>
--- static/js/register.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('.datepicker').datepicker();
-});
-
-document.getElementById("formwrap").addEventListener("submit",function(e){
-    e.preventDefault();
-    window.location.href = "ulogin.php"
-});
