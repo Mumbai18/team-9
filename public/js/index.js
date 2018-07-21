a2373ea8e11d0ab1770a06055f47d25433d4516d
Yadniki <yadnikip7@gmail.com>
2018-07-21 17:44:10 +0530

    static files

---
+++ public/js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $(".dropdown-trigger").dropdown();
+    $('.parallax').parallax();
+    console.log('Script called');
+});
+
+// function home(){
+//     document.addEventListener('DOMContentLoaded', function() {
+//         var elems = document.querySelectorAll('.parallax');
+//         var instances = M.Parallax.init(elems, options);
+//       });
+    
+//     document.addEventListener('DOMContentLoaded', function() {
+//         var elems = document.querySelectorAll('.dropdown-trigger');
+//         var instances = M.Dropdown.init(elems, options);
+//       });
+    
+// }
+
+
