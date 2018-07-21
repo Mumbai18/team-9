80babd4bc41b88055c454b6e691900356a7fd866
Riya Karia <2015riya.karia@ves.ac.in>
2018-07-22 00:33:57 +0530

    connect

--- New/connect.php
+++ New/connect.php
@@ -1,5 +1,8 @@
 <?php 
 $connect_error='Sorry';
 
+
 $con=mysqli_connect("localhost:3307","root","","educon")or die($connect_error);
+//$con=mysqli_connect("localhost","root","","educon")or die($connect_error);
+
  ?>
