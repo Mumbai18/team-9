69d428b80240c30d61df1ef90430c3e112d4de4a
Abhijeet Salunkhe <abhijeet21895@gmail.com>
2018-07-21 17:39:37 +0530

    Action_page Updated
    updated the Action page with sign up informtaion

--- action_page.php
+++ action_page.php
@@ -3,11 +3,12 @@ include_once 'connect.php';
 
 $email=$_GET['email'];
 $password=$_GET['psw'];
-$repeat_pass=$_GET['psw-repeat'];
+$fname=$_GET['fname'];
+$mobile=$_GET['mobile'];
 
 if(isset($email) || isset($password))
 {
-	$sql="INSERT into `stud_registration` (`stud_email`,`stud_pwd`) values ($email,$password)";
+	$sql="INSERT into `stud_registration` (`stud_fname`,`stud_mobile`,`stud_email`,`stud_pwd`) VALUES ($fname,$mobile,$email,$password)";
 	$result=mysqli_query($con,$sql);
       
       if($result)
